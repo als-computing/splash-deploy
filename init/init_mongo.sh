6e9a49f0de2aeb960882c024781c5dabab33d2f6
dylanmcreynolds <dmcreynolds@lbl.gov>
2019-08-27 10:31:28 -0700

    Change name to match services in docker-copose.yml

--- init/init_mongo.sh
+++ init/init_mongo.sh
@@ -1,9 +1,9 @@
 docker-compose up -d
 
-docker exec -it splash_db0_1 /bin/bash
+docker exec -it splash_db_1 /bin/bash
 
 
-echo mongo --host db0:27017 
+echo mongo --host db:27017 
 
 echo above will get you logged into the mongo command line, but
 echo you need to manually type after:
@@ -15,7 +15,7 @@ var cfg = {
   "version": 1,
   "members" : [
    {"_id": 1, "host": "db:27017"}
-  ],settings: {chainingAllowed: true}
+  ]
  }
 
  rs.initiate(cfg, { force: true });
