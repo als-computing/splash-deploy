64e2344569f76e599f41d24da38e065b4f655e7c
Joshua Avery <joshuaaver01@gmail.com>
2020-07-31 16:03:39 -0700

    Works with run start documents

--- init/python/config.py
+++ init/python/config.py
@@ -1,9 +1,202 @@
 elastic = {
     "url": "http://localhost:80/search/",
-    "alias": "experiments1",
+    "alias": "run_start",
+    "index_name": "splash.run_start"
 }
 
-db = {
-    "name": "splash",
-    "collection": "experiments",
+# db = {
+#    "name": "splash",
+#    "collection": "experiments",
+# }
+
+
+# this is the configuration for the new index
+# find documentation on this here
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html
+# So far the only configuration I have done is set up autocompletion
+# and keyword on select fields
+INDEX_CONFIG = {
+    # This setting makes it so that if a field in a document doesn't match the set datatype then
+    # elastic will still index the entire document but ignore that field. This
+    # doesn't work for the Nested data type, object data type, or range data type
+    "settings": {"index.mapping.ignore_malformed": True},
+    "mappings": {
+        "properties": {
+            # this is autocompletable, full text searchable, and keyword searchable
+            "project": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword"
+                    }
+                }
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "sample": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword",
+                    }
+                }
+            },
+            # this is simply an integer field
+            "scan_id": {
+                "type": "integer"
+            },
+            # this is a date field which will be useful for
+            # certain queries
+            "time": {
+                "type": "date"
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "uid": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword"
+                        }
+                }
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "group": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword"
+                        }
+                }
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "owner": {
+                "type": "text",
+                "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                        },
+                    "autocomplete": {
+                            "type": "completion"
+                        }
+                }
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "beamline_id": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword"
+                        }
+                }
+            },
+            # this is autocompletable, full text searchable, and keyword searchable
+            "user": {
+                "type": "text",
+                "fields": {
+                        "autocomplete": {
+                            "type": "completion"
+                        },
+                    "keyword": {
+                            "type": "keyword"
+                        }
+                }
+            }
+        },
+        # These templates map based on the datatype of the
+        "dynamic_templates": [
+            {
+                # This turns the datatype long into strings when indexed
+                "long_to_string": {
+                    "match_mapping_type": "long",
+                    "mapping": {
+                        "type": "text",
+                        "fields": {
+                                "keyword": {
+                                    "type":  "keyword",
+                                    # This means that if the character count on the
+                                    # text field is above 256 characters then there will
+                                    # no keyword field indexed on this field
+                                    "ignore_above": 256
+                                },
+                        }
+                    }
+                },
+            },
+
+            {
+                "boolean_to_string": {
+                    "match_mapping_type": "boolean",
+                    "mapping": {
+                        "type": "text",
+                        "fields": {
+                                "keyword": {
+                                    "type":  "keyword",
+                                    "ignore_above": 256
+                                },
+                        }
+                    }
+                },
+            },
+
+            {
+                "date_to_string": {
+                    "match_mapping_type": "date",
+                    "mapping": {
+                        "type": "text",
+                        "fields": {
+                                "keyword": {
+                                    "type":  "keyword",
+                                    "ignore_above": 256
+                                },
+                        }
+                    }
+                },
+            },
+
+            {
+                "double_to_string": {
+                    "match_mapping_type": "double",
+                    "mapping": {
+                        "type": "text",
+                        "fields": {
+                                "keyword": {
+                                    "type":  "keyword",
+                                    "ignore_above": 256
+                                },
+                        }
+                    }
+                },
+            },
+
+            {
+                "strings": {
+                    "match_mapping_type": "string",
+                    "mapping": {
+                        "type": "text",
+                        "fields": {
+                                "keyword": {
+                                    "type":  "keyword",
+                                    "ignore_above": 256
+                                }
+                        }
+                    }
+                }
+            }
+        ]
+    },
 }
+
--- init/python/setup.py
+++ init/python/setup.py
@@ -1,21 +1,27 @@
 from elastic_setup.elastic import elastic_setup
-from mongo_setup import generate_json, import_json
+# from mongo_setup import generate_json, import_json
 import config
 
 
-DB_NAME = config.db['name']
+# DB_NAME = config.db['name']
+# DB_COLLECTION = config.db['collection']
+
 ALIAS = config.elastic['alias']
 ELASTIC_URL = config.elastic['url']
-DB_COLLECTION = config.db['collection']
-ELASTIC_INDEX_NAMED_BY_MONSTACHE = config.db['name'] + "." + \
-    config.db['collection']
+ELASTIC_INDEX_NAMED_BY_MONSTACHE = config.elastic['index_name']
 
 
 # IMPORTANT: Elastic must be setup before Mongo
 #  so that monstache can transfer the data
 elastic_setup(host_url=ELASTIC_URL,
-              index_name=ELASTIC_INDEX_NAMED_BY_MONSTACHE, alias=ALIAS)
+              index_name=ELASTIC_INDEX_NAMED_BY_MONSTACHE,
+              index_config=config.INDEX_CONFIG,
+              alias=ALIAS)
 
-generate_json.generate()
 
-import_json.upload(db_name=DB_NAME, collection_name=DB_COLLECTION)
+# uncomment ONLY if you wish to generate fake data for mongo,
+# Probably don't want to do this because it will get mixed up
+# in the real data
+# generate_json.generate()
+
+# import_json.upload(db_name=DB_NAME, collection_name=DB_COLLECTION)
